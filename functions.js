b50c813707164e0f22e8fe459afddcfe3b276d8e
Gavin Carberry <98050479+GavDotZip@users.noreply.github.com>
2023-04-24 16:02:26 +0100

    new commit

---
+++ functions.js
@@ -0,0 +1,83 @@
+// Associate image with the correct answer for the image
+const questions = [
+    {
+        image: "images/Alakazam.jpg",
+        correctAns: "Alakazam",
+    },
+
+    {
+        image: "images/Arcanine.jpg",
+        correctAns: "Arcanine",
+    },
+
+    {
+        image: "images/Bulbasaur.jpg",
+        correctAns: "Bulbasaur",
+    },
+
+    {
+        image: "images/Cubone.jpg",
+        correctAns: "Cubone",
+    },
+
+    {
+        image: "images/Ditto.jpg",
+        correctAns: "Ditto",
+    },
+
+    {
+        image: "images/Gloom.jpg",
+        correctAns: "Gloom",
+    },
+
+    {
+        image: "images/Gyarados.jpg",
+        correctAns: "Gyarados",
+    },
+
+    {
+        image: "images/Hitmonlee.jpg",
+        correctAns: "Hitmonlee",
+    },
+
+  {
+        image: "images/Horsea.jpg",
+        correctAns: "Horsea",
+  },
+
+  {
+        image: "images/Koffing.jpg",
+        correctAns: "Koffing",
+  },
+
+  {
+        image: "images/Mewtwo.jpg",
+        correctAns: "Mewtwo",
+  },
+
+  {
+        image: "images/Seaking.jpg",
+        correctAns: "Seaking",
+  },
+
+  {
+        image: "images/Tauros.jpg",
+        correctAns: "Tauros",
+  },
+
+  {
+        image: "images/Venonat.jpg",
+        correctAns: "Venonat",
+  },
+
+  {
+        image: "images/Victreebe.jpg",
+        correctAns: "Victreebe",
+  },
+
+  {
+        image: "images/eevee.jpg",
+        correctAns: "Eevee",
+  },
+
+]; //end questions
